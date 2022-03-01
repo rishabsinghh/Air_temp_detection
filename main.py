4594dc7f75f401fabb18036d35431532a05f58b4
rishabsinghh <45711727+rishabsinghh@users.noreply.github.com>
2022-03-01 14:48:10 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb  3 21:48:50 2022
+
+@author: singh-93
+"""
+import numpy as np
+from flask import Flask, request, jsonify, render_template
+import pickle
+import os
+
+app = Flask(__name__)
+model = pickle.load(open(os.getcwd()+'\\'+'models'+'\\'+'Linear_Regression'+'\\'+'linear_regression.sav', 'rb'))
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/predict',methods=['POST'])
+def predict():
+
+    int_features = [float(x) for x in request.form.values()]
+    final_features = [np.array(int_features)]
+    prediction = model.predict(final_features)
+
+    output = round(prediction[0], 2)
+
+    return render_template('index.html', prediction_text='Air Temperature should be {}'.format(output))
+
+@app.route('/results',methods=['POST'])
+def results():
+
+    data = request.get_json(force=True)
+    prediction = model.predict([np.array(list(data.values()))])
+
+    output = prediction[0]
+    return jsonify(output)
+
+if __name__ == "__main__":
+    app.run(debug=True)
